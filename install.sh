1e601c8f2f193174f2144ed1f3f87ff8ede685cf
Jayanta Kurumathur <jayantak283@gmail.com>
2023-01-29 17:08:50 +0100

    Install python dependencies

--- install.sh
+++ install.sh
@@ -1,3 +1,33 @@
 #!/usr/bin/bash
 
-echo 'Installed all applications'
+echo "Installing Python virtual environments..."
+
+pip3 install virtualenv virtualenvwrapper
+if ! grep -q "#Virtualenvwrapper settings" ; then
+    cat config/venvrc.j2 >> $HOME/.bashrc
+    source $HOME/.bashrc
+fi
+mkvirtualenv main
+workon main
+
+echo "Installing Jinja..."
+
+pip3 install Jinja2
+
+echo "Installed Jinja2 ✅"
+
+echo "Installing Visual Studio Code..."
+
+sudo apt update
+sudo apt install code
+
+echo "Installed Visual Studio Code ✅"
+
+echo "Installing smtp client..."
+
+sudo apt-get install msmtp msmtp-mta --assume-yes
+sudo cp config/msmtprc.j2 /etc/msmtprc
+
+echo "Installed smtp client ✅"
+
+echo "Installed all applications"
